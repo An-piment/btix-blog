8aac0381ce348875f92ed6fdfa9d7530a7deb702
piments <an.piment@terra.com.br>
2022-11-24 19:13:19 -0300

    Adicionado Título e logo da btix

--- src/components/title/Title.jsx
+++ src/components/title/Title.jsx
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import styles from './Title.module.css'
+import logo from './logo.png';
 
-class Titles extends Component {
+class Title extends Component {
 	render() {
-		return;
+		return (
+			<div className={styles.mainBox}>
+				<img className={styles.logo} src={logo} alt="Btix Logo" />
+				<h1>{this.props.children}</h1>
+				<img className={styles.logo} src={logo} alt="Btix Logo" />
+			</div>
+		)
 	}
 }
 
-export default Titles;
+export default Title;
