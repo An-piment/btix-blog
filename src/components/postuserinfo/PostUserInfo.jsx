a7e537bb7428350b21403ba9268e8b405d09201e
piments <an.piment@terra.com.br>
2022-11-24 23:35:48 -0300

    Adicionado informações do usuário que postou a mensagem

---
+++ src/components/postuserinfo/PostUserInfo.jsx
@@ -0,0 +1,13 @@
+import styles from './PostUserInfo.module.css';
+import userIcon from './user-icon.svg';
+
+const PostUserInfo = ({ userName }) => {
+		return (
+			<p className={styles.postHeaderParagraph}>
+				<img className={styles.userIcon} src={userIcon} alt="User Profile" />
+				<span className={styles.userName}>{userName}</span>
+			</p>
+		)
+	}
+
+export default PostUserInfo;
